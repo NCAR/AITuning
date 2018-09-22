431dcffbe37c247a9403ae093be1d33918379c76
Alessandro Fanfarillo <elfanfa@ucar.edu>
2018-09-22 16:38:37 -0600

    Python script quasi working (model is not defined)

--- MPICH_DQN_parabola_Q_learning.py
+++ MPICH_DQN_parabola_Q_learning.py
@@ -55,7 +55,19 @@ def _read_control_vars(raw_data):
 def write_changes(changes):
     file_object  = open('changes.txt', 'w')
     for i in changes:
-        file_object.write(str(changes[i])+" ")
+        file_object.write(str(changes[i])+"\n")
+
+# def read_replay(X,Y):
+    
+        
+def write_replay(X,Y):
+    file_object_X = open('replay_X.txt', 'w')
+    for i in X:
+        file_object_X.write(str(X[i])+"\n")
+
+    file_object_Y = open('replay_Y.txt', 'w')
+    for i in Y:
+        file_object_Y.write(str(Y[i])+"\n")
 
 def read_control_vars():
     return _read_control_vars(open('control_variables.txt', 'r').read())
@@ -63,29 +75,16 @@ def read_control_vars():
 def read_performance_vars():
     return _read_performance_vars(open('performance_variables.txt', 'r').read())
 
+def read_old_performance_vars():
+    return _read_performance_vars(open('old_performance_variables.txt', 'r').read())
+
 def read_counter():
     counter = open("counter.txt", 'r').read()
     return int(counter)
 
-def save_counter(counter):
+def write_counter(counter):
     fileout = open("counter.txt", 'w')
     fileout.write(str(counter))
-    
-def simulate_execution(control_vars, performance_vars):
-    global n_performance_vars
-    global n_control_vars
-    global static_control_vars
-    
-    randomness = np.random.rand(1)
-    new_perf_vars = np.zeros(n_performance_vars)
-    new_perf_vars[:] = performance_vars[:]
-
-    new_perf_vars[n_performance_vars - 1] = (control_vars[n_control_vars - 1] - 200.) ** 2 + randomness
-    new_perf_vars[n_performance_vars - 2] = (control_vars[n_control_vars - 2] - 20.) ** 2 + randomness
-    new_perf_vars[n_performance_vars - 3] = (control_vars[n_control_vars - 3] - 15.) ** 2 + randomness
-    new_perf_vars[n_performance_vars - 4] = (control_vars[n_control_vars - 4] - 156.) ** 2 + randomness
-    
-    return np.floor(new_perf_vars)
 
 def check_reward(performance_vars, new_perf_vars):
     reward = 0
@@ -98,7 +97,7 @@ def check_reward(performance_vars, new_perf_vars):
             reward = reward - 1
     return reward
 
-def make_epsilon_greedy_policy(Q,epsilon,n_actions):
+def make_epsilon_greedy_policy(model,epsilon,n_actions):
     
     def policy_fn(state):
         A = np.ones(n_actions, dtype=float) * epsilon / (n_actions)
@@ -121,77 +120,81 @@ def get_Q_value_all_actions(X):
     Y = model.predict(X)
     return Y
 
+def fromDictToNp(dict):
+    alist = [int(v) for k,v in dict.items()]
+    X = np.zeros(len(alist))
+    for i in range(len(X)):
+        X[i] = int(alist[i])
+    return X
+
 def main():
 
     counter = -1
     replay_X = []
     replay_Y = []
 
-    if(os.path.isfile('model.json'):
-       json_file = open('model.json', 'r')
-       loaded_model_json = json_file.read()
-       json_file.close()
-       model = model_from_json(loaded_model_json)
-       # load weights into new model
-       model.load_weights("model.h5")
-       print("Loaded model from disk")
-       # Read replay and counter from disk
-    else:
-       model = Sequential()
-       model.add(Dense(25, input_dim=n_performance_vars, activation='relu'))
-       model.add(Dense(50, kernel_initializer='random_normal',
-                       bias_initializer='zeros', activation='relu'))
-       model.add(Dense(100, kernel_initializer='random_normal',
-                       bias_initializer='zeros',activation='relu'))
-       model.add(Dense(100, kernel_initializer='random_normal',
-                       bias_initializer='zeros',activation='relu'))
-       model.add(Dense(n_actions, kernel_initializer='zeros',
-                       bias_initializer='zeros',activation='relu'))
-       model.add(Dense(n_actions, activation='linear'))
-       
-    adam = optimizers.Adam(lr=0.02, beta_1=0.9, beta_2=0.999, epsilon=1E-8, decay=0.001, amsgrad=False)
-    
-    model.compile(loss='mse', optimizer=adam, metrics=['accuracy'])
-
-    performance_vars = read_performance_vars()
+    performance_vars = read_old_performance_vars()
     n_performance_vars = len(performance_vars)
     control_vars = read_control_vars()
     n_control_vars = len(control_vars)
     #static_control_vars = np.ones(n_control_vars)
     n_actions = n_control_vars * 2 + 1 #the +1 is for "do nothing" action
+    changes = np.zeros(n_control_vars)
+
+    if(os.path.isfile('model.json')):
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        model = model_from_json(loaded_model_json)
+        # load weights into new model
+        model.load_weights("model.h5")
+        print("Loaded model from disk")
+        # Read replay and counter from disk
+    else:
+        model = Sequential()
+        model.add(Dense(25, input_dim=n_performance_vars, activation='relu'))
+        model.add(Dense(50, kernel_initializer='random_normal',
+                        bias_initializer='zeros', activation='relu'))
+        model.add(Dense(100, kernel_initializer='random_normal',
+                        bias_initializer='zeros',activation='relu'))
+        model.add(Dense(100, kernel_initializer='random_normal',
+                        bias_initializer='zeros',activation='relu'))
+        model.add(Dense(n_actions, kernel_initializer='zeros',
+                        bias_initializer='zeros',activation='relu'))
+        model.add(Dense(n_actions, activation='linear'))
+        
+    adam = optimizers.Adam(lr=0.02, beta_1=0.9, beta_2=0.999, epsilon=1E-8, decay=0.001, amsgrad=False)
+    
+    model.compile(loss='mse', optimizer=adam, metrics=['accuracy'])
 
     epsilon = 0.1
     alpha = 0.5
     discount_factor = 1.0
-    
-    # control_vars = np.ones(n_control_vars)
-    # performance_vars = np.ones(n_performance_vars)
-    # action_frequency = np.zeros(n_actions)
-    
-    # for i in range(n_episodes):
+
+    counter = 0#read_counter()
     counter = counter + 1
-    if(counter == 200):
-       lb = random.randint(0, int(len(replay_X)/4))
-       ub = random.randint(int(len(replay_X)/4), int(len(replay_X)/4)*3)
-       print(lb,ub)
-       model.fit(np.array(replay_X[lb:ub]),np.array(replay_Y[lb:ub]),verbose=0)
-       counter = -1
-    policy = make_epsilon_greedy_policy(Q,epsilon,n_actions)
-    state = np.floor(performance_vars)
+    # if(counter == 200):
+    #    lb = random.randint(0, int(len(replay_X)/4))
+    #    ub = random.randint(int(len(replay_X)/4), int(len(replay_X)/4)*3)
+    #    print(lb,ub)
+    #    model.fit(np.array(replay_X[lb:ub]),np.array(replay_Y[lb:ub]),verbose=0)
+    #    counter = -1
+    policy = make_epsilon_greedy_policy(model, epsilon, n_actions)
+    np_performance_vars = fromDictToNp(performance_vars)
+    state = np.floor(np_performance_vars)
     total_reward = 0
-        # for j in range(n_steps):
     action_probs = policy(np.array([state]))
     action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
     if(int(action/2) >= n_control_vars):
-       pass
+        pass
     elif(action%2 == 0):
-       control_vars[int(action/2)] = control_vars[int(action/2)] - 1
+        changes[int(action/2)] = -1
     else:
-       control_vars[int(action/2)] = control_vars[int(action/2)] + 1
+        changes[int(action/2)] = 1
     # new_perf_vars = simulate_execution(control_vars, performance_vars)
-    new_perf_vars = read_performance_vars()
-    reward = check_reward(performance_vars, new_perf_vars)
-    performance_vars[:] = new_perf_vars[:]
+    new_perf_vars = fromDictToNp(read_performance_vars())
+    reward = 10#check_reward(performance_vars, new_perf_vars)
+    np_performance_vars[:] = new_perf_vars[:]
     next_state = np.floor(new_perf_vars)
     next_action = select_next_action(np.array([next_state]))
     # Update policy
@@ -207,15 +210,13 @@ def main():
     Y_all[0][action] = Y
     replay_X.extend(X)
     replay_Y.extend(Y_all)
+    #write_replay(replay_X, replay_Y)
     model.fit(X,Y_all,verbose=0)
-    state[:] = next_state[:]
-    action_frequency[action] += 1
-        
-    print("total reward",total_reward)#,"action frequency",action_frequency)
+    write_counter(counter)
 
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
-       json_file.write(model_json)
+        json_file.write(model_json)
     # serialize weights to HDF5
     model.save_weights("model.h5")
     print("Saved model to disk")
